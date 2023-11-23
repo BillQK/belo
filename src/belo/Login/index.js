986923f244bc35d39a6f5c77ebc52b3563bab308
BillQK <billngo47@gmail.com>
2023-11-23 02:17:46 -0500

    client.js

--- src/belo/Login/index.js
+++ src/belo/Login/index.js
@@ -2,10 +2,35 @@ import React from "react";
 import Button from "../components/Button/Button";
 
 import { loginEndpoint } from "../../spotify";
-
+import * as client from "../Services/client";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 const Login = () => {
+  const [credentials, setCredentials] = useState({
+    username: "",
+    password: "",
+  });
+  const navigate = useNavigate();
+  const signin = async () => {
+    await client.signin(credentials);
+    navigate("/project/account");
+  };
   return (
     <div className="Login-page">
+      <h1>Signin</h1>
+      <input
+        value={credentials.username}
+        onChange={(e) =>
+          setCredentials({ ...credentials, username: e.target.value })
+        }
+      />
+      <input
+        value={credentials.password}
+        onChange={(e) =>
+          setCredentials({ ...credentials, password: e.target.value })
+        }
+      />
+      <button onClick={signin}> Signin </button>
       <a href={loginEndpoint}>
         <Button text="Login" />
       </a>
