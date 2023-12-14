5ad7b4061643c41095e96fa9d333e68b6e1a9270
BillQK <billngo47@gmail.com>
2023-12-13 19:08:24 -0500

    update style for user status

--- src/belo/User/userStatus.js
+++ src/belo/User/userStatus.js
@@ -1,13 +1,15 @@
 import { useEffect, useState } from "react";
-import { FiLogOut } from "react-icons/fi";
+import { FiLogOut, FiLogIn } from "react-icons/fi"; // Import for login/logout icons
 import "./userStatus.css";
-import * as profileClient from "../Services/profilesClient"; // Import the profile client
+import * as profileClient from "../Services/profilesClient";
 import * as userClient from "../Services/userClient";
 import { useNavigate } from "react-router";
+
 const UserStatus = ({ user }) => {
   const navigate = useNavigate();
   const [profile, setProfile] = useState(null);
   const [isLoading, setIsLoading] = useState(true);
+
   const signout = async () => {
     try {
       const status = await userClient.signOut();
@@ -18,34 +20,44 @@ const UserStatus = ({ user }) => {
       console.log(error);
     }
   };
+
+  const login = () => {
+    navigate("/Register/Login"); // Navigate to login page
+  };
+
   useEffect(() => {
     if (user) {
       const fetchData = async () => {
         try {
           const userProfile = await profileClient.getProfileByUserID(user._id);
           setProfile(userProfile);
-          setIsLoading(false); // Set loading to false when data is fetched
         } catch (error) {
           console.error("Error fetching user profile:", error);
-          setIsLoading(false); // Set loading to false even on error
+        } finally {
+          setIsLoading(false); // Set loading to false when done
         }
       };
       fetchData();
+    } else {
+      setIsLoading(false); // If there's no user, don't show loading
     }
   }, [user]);
 
+  if (isLoading) {
+    // Optionally, you can display a loader or a blank div here
+    return <div className="container logout">Loading...</div>;
+  }
+
   return (
     <div className="container logout">
-      {isLoading ? (
-        <p>Anonymously</p>
-      ) : user ? (
+      {user && profile ? (
         <>
           {/* Display user profile data */}
           <div>
             <img src={profile.avatar} alt="Avatar" />
           </div>
           <div>
-            <h1>{profile ? profile.userName : ""}</h1>
+            <h1>{profile.userName}</h1>
             <h2>Logged In</h2>
           </div>
           <div>
@@ -53,7 +65,12 @@ const UserStatus = ({ user }) => {
           </div>
         </>
       ) : (
-        <p>User is not logged in.</p>
+        <>
+          <p>User is not logged in.</p>
+          <div>
+            <FiLogIn onClick={login} />
+          </div>
+        </>
       )}
     </div>
   );
