0371091625d1a56da2bc1f4911b0361943228d44
BillQK <billngo47@gmail.com>
2023-12-15 18:23:52 -0500

    10 s timeout

--- src/belo/components/Post/PostComponents/SpotifyIFrame.js
+++ src/belo/components/Post/PostComponents/SpotifyIFrame.js
@@ -27,7 +27,7 @@ const SpotifyIframe = ({ contentType, contentID }) => {
       if (isLoading) {
         setError(true);
       }
-    }, 200); // 3 seconds timeout
+    }, 10000); // 10 seconds timeout
 
     return () => clearTimeout(loadTimeoutRef.current);
   }, [isLoading]);
